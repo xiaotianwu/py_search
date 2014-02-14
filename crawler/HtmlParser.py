97cb5f0d5fcbca3dde1b52f7e47cc4900d8f39a7
xiaotianwu <wxt1984@gmail.com>
2014-02-13 19:57:31 -0800

    refine html parser

--- crawler/HtmlParser.py
+++ crawler/HtmlParser.py
@@ -4,27 +4,34 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 from HTMLParser import HTMLParser
 
-# Import stop words
-#class Preprocessor:
-#    def __init__(self):
-#        pass
-
 # a simple wordbreaker based on space splitting, will be moved to common folder later
-class WordBreaker:
+class SimpleWordBreaker:
     def __init__(self):
         pass
 
     def split(self, sentence):
-        return [word.strip() for word in sentence.split(' ') if word.strip() != '']
+        return sentence.split(' ')
 
 class ExtendedHTMLParser(HTMLParser):
     debugOutput = False
-    link = []
-    term = []
+    link = set()
+    term = set()
+    stopwords = set()
     tagClassification = 'UNKNOWN'
     linkTagRegex = re.compile('(a)|(base)')
     textTagRegex = re.compile('(p)|(h[1-6])|(em)')
-    wordBreaker = WordBreaker()
+    regularWordRegex = re.compile('[a-zA-Z]+')
+    wordBreaker = SimpleWordBreaker()
+
+    def set_stopwords(self, stopwordsFile):
+        languageSectionRegex = re.compile('\[[a-z][a-z]\-[a-z][a-z]\]')
+        for line in open(stopwordsFile, 'r').readlines():
+            if len(line) == 0 or line[0] == '#' or languageSectionRegex.match(line):
+                pass
+            self.stopwords.add(line.replace('\r', '').replace('\n', '').lower())
+
+    def set_wordbreaker(self, wb):
+        self.wordBreaker = wb
 
     def set_debug(self, debug):
         self.debugOutput = debug
@@ -36,7 +43,7 @@ class ExtendedHTMLParser(HTMLParser):
             else:
                 for (name, value) in attrs:
                     if name == 'href':
-                        self.link.append(value)
+                        self.link.add(value)
                 self.tagClassification = 'LINK'
                 if self.debugOutput == True:
                     print "Tag =", tag
@@ -48,11 +55,22 @@ class ExtendedHTMLParser(HTMLParser):
                 print "Attrs =", attrs
         else:
             self.tagClassification = 'UNKNOWN'
-            
+
+    def filter_word(self, word):
+        word = word.strip().lower()
+        if len(word) == 0 or word in self.stopwords:
+            return ''
+        elif not self.regularWordRegex.match(word):
+            return ''
+        else:
+            return word
 
     def handle_data(self, data):
         if self.debugOutput == True:
             print "Data =", data
         if self.tagClassification in ('LINK', 'TEXT'):
             words = self.wordBreaker.split(data);
-            self.term += words
+            for word in words:
+                word = self.filter_word(word)
+                if word != '':
+                    self.term.add(word)
--- crawler/HtmlParserTest.py
+++ crawler/HtmlParserTest.py
@@ -3,8 +3,10 @@
 from HtmlParser import ExtendedHTMLParser
 
 parser = ExtendedHTMLParser();
+parser.set_stopwords('StopWordsList.txt')
 parser.feed(open('Sample.yahoo.html','r').read())
 
 print '----------------Split---------------------'
+print 'stop words = ', parser.stopwords
 print 'link list =', parser.link
 print 'term list =', parser.term
