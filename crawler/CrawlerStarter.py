180fcc85f776e76e6e6c68fd1c54c067c6b18a8c
xiaotianwu <wxt1984@gmail.com>
2014-02-16 14:44:58 -0800

    modify CrawlerStarter

--- crawler/CrawlerStarter.py
+++ crawler/CrawlerStarter.py
@@ -15,9 +15,12 @@ if not os.path.exists(pageChunkPath):
 
 urlRegex = [re.compile('.*\.yahoo\.com/.*')]
 threads = [UrlCrawlingThread(debug = True) for i in range(0, 5)]
+seedUrls = ['http://news.yahoo.com', 'http://sports.yahoo.com', 'http://finance.yahoo.com', 'http://weather.yahoo.com', 'http://games.yahoo.com']
+i = 0
 for t in threads:
-    t.init('http://www.yahoo.com', pagesLimit = 1000, crawlInterval = 1, timeout = 5, urlFilterRegexCollection = urlRegex)
+    t.init(seedUrls[i], pagesLimit = 1000, crawlInterval = 1, timeout = 5, urlFilterRegexCollection = urlRegex)
     t.start()
+    i += 1
 
 for t in threads:
     t.join()
