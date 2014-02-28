30206e66ad8fc5fb94399aabd249a41e10743680
xiaotianwu <wxt1984@gmail.com>
2014-02-28 04:59:40 -0800

    add event for DiskIOManager

--- common/DiskIOManager.py
+++ common/DiskIOManager.py
@@ -1,10 +1,10 @@
-#import threading
-import time
+from threading import Event
 from Queue import Queue
 from multiprocessing.pool import ThreadPool as Pool
 
 #from gevent.pool import Pool
 #from gevent.queue import Queue
+#from gevent.event import Event
 
 from Logger import Logger
 
@@ -19,23 +19,18 @@ class DiskIORequest:
         self.retCode = 0 # TODO use enum to replace it
 
 class DiskIOManager:
-    def __init__(self, diskIOThreadNum = 5, maxTask = 30):
+    def __init__(self, diskIOThreadNum = 5, maxTask = 100):
         self._ioRequestQueue = Queue()
-        #self._ioThreads = Pool(processes = diskIOThreadNum)
         self._ioThreads = Pool(diskIOThreadNum)
-        # TODO use LRU to cache fileDesc when opened file is more than 1024
+        # TODO Need LRU Strategy Cache?
         self._fileDescCache = {}
         self._ioCompleteSet = {}
         self._logger = Logger.get('IOThread')
-        self._curTask = 0
-        self._logger.debug('start manager')
-        self._maxTask = 30
+        self._finishedTaskNum = 0
+        self._maxTaskNum = maxTask
         
-    def _stop(self):
-        #self._ioThreads.close()
-        self._logger.debug('exit manager')
-
     def run(self):
+        self._logger.info('start manager')
         while True:
             ioRequest = self._ioRequestQueue.get()
             if ioRequest.Type == 'STOP':
@@ -45,19 +40,29 @@ class DiskIOManager:
                 if ioRequest.fileName not in self._fileDescCache:
                     name = ioRequest.fileName
                     self._fileDescCache[name] = open(name, 'r')
-                #self._logger.debug('enter read thread')
+                self._logger.debug('enter read thread')
                 self._ioThreads.apply_async(self._read_from_disk, (ioRequest,))
             elif ioRequest.Type == 'WRITE':
                 continue
             else:
                 continue
 
+    def post_stop_request(self):
+        request = DiskIORequest(-1, 'STOP', '', -1, -1)
+        self._ioRequestQueue.put(request)
+
+    def post_diskio_request(self, ioRequest):
+        '''return event which can be waited'''
+        self._ioRequestQueue.put(ioRequest)
+        self._ioCompleteSet[ioRequest.Id] = Event()
+        return self._ioCompleteSet[ioRequest.Id]
+
     def _read_from_disk(self, ioRequest):
-        # TODO generate debugString only when the debug mode is enable
-        #debugString = 'start reading, fileName = ' + ioRequest.fileName +\
-        #              ', offset = ' + str(ioRequest.offset) +\
-        #              ', len = ' + str(ioRequest.length)
-        #self._logger.debug(debugString)
+        # TODO concat debugString only when debug is enable
+        debugString = 'start reading, fileName = ' + ioRequest.fileName +\
+                      ', offset = ' + str(ioRequest.offset) +\
+                      ', len = ' + str(ioRequest.length)
+        self._logger.debug(debugString)
 
         fd = self._fileDescCache[ioRequest.fileName]
         fd.seek(ioRequest.offset)
@@ -65,18 +70,19 @@ class DiskIOManager:
             data = fd.read()
         else:
             data = fd.read(ioRequest.length)
-        #self._logger.debug('finished reading')
-        self._curTask += 1
-        if self._curTask == self._maxTask:
-            self._logger.debug('finish all task')
-            self.post_close_request()
+        self._logger.debug('finished reading')
+
+        self._finishedTaskNum += 1
+        if self._finishedTaskNum == self._maxTaskNum:
+            self._logger.info('finish all task')
+            self.post_stop_request()
 
         ioRequest.result = data
-        self._ioCompleteSet[ioRequest.Id] = ioRequest
+        self._ioCompleteSet[ioRequest.Id].set()
 
-    def post_close_request(self):
-        request = DiskIORequest(-1, 'STOP', '', -1, -1)
-        self._ioRequestQueue.put(request)
-
-    def post_diskio_request(self, request):
-        self._ioRequestQueue.put(request)
+    def _stop(self):
+        # TODO it's necessary to stop the threadPools
+        self._ioThreads.close()
+        for desc in self._fileDescCache.values():
+            desc.close()  
+        self._logger.info('exit manager')
--- common/DiskIOManagerTest.py
+++ common/DiskIOManagerTest.py
@@ -1,12 +1,63 @@
 #!/usr/bin/python
 
-from DiskIOManager import *
-import time
 import os
+import time
+#import gevent
+import threading
+
+from DiskIOManager import *
+
+class DiskIOManagerTestThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self._manager = DiskIOManager()
+
+    def run(self):
+        self._manager.run() 
+
+    def post_diskio_request(self, request):
+        return self._manager.post_diskio_request(request)
+
+    def post_stop_request(self):
+        self._manager.post_stop_request()
+
+def create_testdata(fileNum):
+    os.system('mkdir testdata')
+    for i in range(0, fileNum):
+        fileName = 'testdata/testFile' + str(i)
+        print('create ' + fileName)
+        os.system('dd of=' + fileName + ' if=/dev/zero bs=1024 count=102400')
+
+def clear_testdata():
+    os.system('rm testdata -r')  
 
 if __name__ == '__main__':
-    dm = DiskIOManager()
+    #create_testdata(30)    
+
+    dmThread = DiskIOManagerTestThread()
+    dmThread.start()
+    
+    #manager = DiskIOManager()
+    #gevent.spawn(manager.run)
+
+    events = []
+
     for i in range(1, 31):
-        r = DiskIORequest(i, 'READ', 'testdata/testfile' + str(i), 0, 1024 * 1024 * 10)
-        dm.post_diskio_request(r)
-    dm.run()
+        req = DiskIORequest(i, 'READ', 'testdata/testfile' + str(i),
+                            0, 1024 * 1024 * 10)
+        ev = dmThread.post_diskio_request(req)
+        #ev = manager.post_diskio_request(req)
+        events.append(ev)
+
+    for ev in events:
+        ev.wait()
+
+    print('all done')
+
+    #manager.post_stop_request()
+    dmThread.post_stop_request()
+
+    #gevent.sleep(5)
+    time.sleep(5)
+
+    #clear_testdata()
