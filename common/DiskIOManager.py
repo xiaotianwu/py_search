ffacdfb44d97bd32007eea43ef759a91ff3c5692
xiaotianwu <wxt1984@gmail.com>
2014-02-28 02:50:06 -0800

    add greenlet test

--- common/DiskIOManager.py
+++ common/DiskIOManager.py
@@ -1,7 +1,10 @@
-import threading
+#import threading
 import time
-import Queue
-from multiprocessing.pool import ThreadPool
+from Queue import Queue
+from multiprocessing.pool import ThreadPool as Pool
+
+#from gevent.pool import Pool
+#from gevent.queue import Queue
 
 from Logger import Logger
 
@@ -15,19 +18,22 @@ class DiskIORequest:
         self.result = None
         self.retCode = 0 # TODO use enum to replace it
 
-class DiskIOManager(threading.Thread):
-    def __init__(self, diskIOThreadNum = 5):
-        threading.Thread.__init__(self)       
-        self._ioRequestQueue = Queue.Queue()
-        self._ioThreads = ThreadPool(processes = diskIOThreadNum)
+class DiskIOManager:
+    def __init__(self, diskIOThreadNum = 5, maxTask = 30):
+        self._ioRequestQueue = Queue()
+        #self._ioThreads = Pool(processes = diskIOThreadNum)
+        self._ioThreads = Pool(diskIOThreadNum)
         # TODO use LRU to cache fileDesc when opened file is more than 1024
         self._fileDescCache = {}
         self._ioCompleteSet = {}
         self._logger = Logger.get('IOThread')
+        self._curTask = 0
+        self._logger.debug('start manager')
+        self._maxTask = 30
         
     def _stop(self):
-        self._ioThreads.close()
-        self._logger.debug('exit thread')
+        #self._ioThreads.close()
+        self._logger.debug('exit manager')
 
     def run(self):
         while True:
@@ -39,7 +45,7 @@ class DiskIOManager(threading.Thread):
                 if ioRequest.fileName not in self._fileDescCache:
                     name = ioRequest.fileName
                     self._fileDescCache[name] = open(name, 'r')
-                self._logger.debug('enter read thread')
+                #self._logger.debug('enter read thread')
                 self._ioThreads.apply_async(self._read_from_disk, (ioRequest,))
             elif ioRequest.Type == 'WRITE':
                 continue
@@ -48,10 +54,10 @@ class DiskIOManager(threading.Thread):
 
     def _read_from_disk(self, ioRequest):
         # TODO generate debugString only when the debug mode is enable
-        debugString = 'start reading, fileName = ' + ioRequest.fileName +\
-                      ', offset = ' + str(ioRequest.offset) +\
-                      ', len = ' + str(ioRequest.length)
-        self._logger.debug(debugString)
+        #debugString = 'start reading, fileName = ' + ioRequest.fileName +\
+        #              ', offset = ' + str(ioRequest.offset) +\
+        #              ', len = ' + str(ioRequest.length)
+        #self._logger.debug(debugString)
 
         fd = self._fileDescCache[ioRequest.fileName]
         fd.seek(ioRequest.offset)
@@ -59,7 +65,11 @@ class DiskIOManager(threading.Thread):
             data = fd.read()
         else:
             data = fd.read(ioRequest.length)
-        self._logger.debug('finished reading')
+        #self._logger.debug('finished reading')
+        self._curTask += 1
+        if self._curTask == self._maxTask:
+            self._logger.debug('finish all task')
+            self.post_close_request()
 
         ioRequest.result = data
         self._ioCompleteSet[ioRequest.Id] = ioRequest
--- common/DiskIOManagerTest.py
+++ common/DiskIOManagerTest.py
@@ -6,15 +6,7 @@ import os
 
 if __name__ == '__main__':
     dm = DiskIOManager()
-    r1 = DiskIORequest(1, 'READ', 'testdata/testfile1', 0, -1)
-    r2 = DiskIORequest(2, 'READ', 'testdata/testfile2', 0, -1)
-    r3 = DiskIORequest(2, 'READ', 'testdata/testfile3', 0, -1)
-    r4 = DiskIORequest(2, 'READ', 'testdata/testfile4', 0, -1)
-    r5 = DiskIORequest(2, 'READ', 'testdata/testfile5', 0, -1)
-    dm.post_diskio_request(r1)
-    dm.post_diskio_request(r2)
-    dm.post_diskio_request(r3)
-    dm.post_diskio_request(r4)
-    dm.post_diskio_request(r5)
-    dm.start()
-    dm.join()
+    for i in range(1, 31):
+        r = DiskIORequest(i, 'READ', 'testdata/testfile' + str(i), 0, 1024 * 1024 * 10)
+        dm.post_diskio_request(r)
+    dm.run()
