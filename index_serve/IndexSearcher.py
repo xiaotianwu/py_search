78b087f268e93233429cc0159600fd0b7753dfc0
xiaotianwu <wxt1984@gmail.com>
2014-03-04 19:27:53 -0800

    add ut for index searcher

--- index_serve/IndexSearcher.py
+++ index_serve/IndexSearcher.py
@@ -2,29 +2,42 @@ import sys
 sys.path.append('../common')
 sys.path.append('../common/SimpleIndex')
 
-import IndexServing
 from SimpleIndex import *
+from Logger import Logger
 
 class IndexSearcher:
     '''the worker to do docid matching'''
     def __init__(self, indexManager):
         self._indexManager = indexManager
+        self._logger = Logger.get('IndexSearcher')
 
     # proc search_perpare -> searching -> search_done TODO make the proc async
     def search(self, termIdList):
-        return search_prepare(termIdList)
+        self._logger.debug('search_prepare, termIdList = ' + str(termIdList))
+        return self.search_prepare(termIdList)
 
     def search_prepare(self, termIdList):
         indexHandler = SimpleIndexHandler() # TODO set a commonHandler
         for termId in termIdList:
-            index, retCode = self._indexManager.fetch(termId) # TODO make fetch async
+            # TODO make fetch async
+            self._logger.debug('fetch term ' + str(termId))
+            index, retCode = self._indexManager.fetch(termId)
             if retCode == True:
+                self._logger.debug('fetch term ' + str(termId) + ' success')
                 indexHandler.add(index)
+            else:
+                self._logger.debug('fetch term ' + str(termId) + ' failed')
+                return self.search_done(set())
+
         return self.searching(indexHandler)
 
     def searching(self, indexHandler):
+        # TODO add unique trailer id
+        self._logger.debug('searching')
         result = indexHandler.intersect()
         return self.search_done(result)
 
     def search_done(self, result):
+        # TODO add unique trailer id
+        self._logger.debug('search done')
         return result
--- index_serve/IndexSearcherTest.py
+++ index_serve/IndexSearcherTest.py
@@ -2,12 +2,13 @@
 
 import os
 import sys
+sys.path.append('../common')
 sys.path.append('../common/SimpleIndex')
 
 from SimpleIndex import SimpleIndex
 from SimpleIndex import SimpleIndexWriter
-
 from IndexManager import IndexManager
+from IndexSearcher import IndexSearcher
 
 indexFileName = 'test.index'
 
@@ -27,21 +28,22 @@ if __name__ == '__main__':
     create_testdata()
 
     indexManager = IndexManager()
-    assert indexManager.is_ready() == False
     indexManager.init(indexFileName)
     assert indexManager.is_ready() == True
 
-    index, retCode = indexManager.fetch(0)
-    assert index == None
-    assert retCode == False
     index, retCode = indexManager.fetch(1) 
     assert index == set([1, 2, 3])
-    assert retCode == True
     index, retCode = indexManager.fetch(2)
     assert index == set([2, 3, 4])
     index, retCode = indexManager.fetch(3)
     assert index == set([3, 4, 5])
     index, retCode = indexManager.fetch(4)
     assert index == set([4, 5, 6])
+    
+    indexSearcher = IndexSearcher(indexManager)
+    assert indexSearcher.search([0, 1]) == set()
+    assert indexSearcher.search([1, 2]) == set([2, 3])
+    assert indexSearcher.search([1, 2, 3]) == set([3])
+    assert indexSearcher.search([1, 2, 3, 4]) == set()
 
     delete_testdata()
