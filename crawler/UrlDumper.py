df97591a9abe765815c944803d3eeaafc50275ce
xiaotianwu <wxt1984@gmail.com>
2014-03-09 01:11:40 -0800

    make UrlDumper available

--- crawler/UrlDumper.py
+++ crawler/UrlDumper.py
@@ -1,45 +1,75 @@
+# leveldb storage is recommended
+USE_LEVELDB = False
+
+import threading
+
 try:
     import leveldb
+    print('use leveldb storage')
     USE_LEVELDB = True
 except ImportError as error:
     print(str(error) + 'use plain storage')
     from common.Common import UrlFileNameConverter as Converter
-    USE_LEVELDB = False
 
 class UrlDumper:
     '''singleton dumper'''
     __db = None
     __path = None
+    __dbLock = threading.RLock()
+    __initialized = False
 
-    def __init__(self, chunkPath):
+    @staticmethod
+    def Init(chunkPath, leveldbFolder):
         global USE_LEVELDB
+        UrlDumper.__dbLock.acquire()
+        if UrlDumper.__initialized == True:
+            UrlDumper.__dbLock.release()
+            return
+        UrlDumper.__path = chunkPath
         if USE_LEVELDB == True and UrlDumper.__db == None:
-            UrlDumper.__path = chunkPath
-            dbFile = UrlDumper.__path + 'urlPages'
-            print(dbFile)
-            UrlDumper.__db = leveldb.LevelDB(dbFile)
+            dbFolder = UrlDumper.__path + '/' + leveldbFolder
+            print('create db foler:' + dbFolder)
+            UrlDumper.__db = leveldb.LevelDB(dbFolder)
+        UrlDumper.__initialized = True
+        UrlDumper.__dbLock.release()
 
-    def Write(self, url, content):
+    @staticmethod
+    def Write(url, page):
+        if UrlDumper.__initialized == False:
+            raise Exception('call init before writing')
         global USE_LEVELDB
+        # leveldb is not thread safe
         if USE_LEVELDB == True:
-            UrlDumper.__db.Put(url, content)
+            UrlDumper.__dbLock.acquire()
+            UrlDumper.__db.Put(url, page)
+            UrlDumper.__dbLock.release()
         else:
             fileName = Converter.UrlToFileName(url)
             try:
-                with open(UrlDumper.__path + fileName, 'w') as pageFile:
+                absolutePath = UrlDumper.__path + '/' + fileName
+                print 'write to file', absolutePath
+                with open(absolutePath, 'w') as pageFile:
                     pageFile.write(page)
             except IOError as error:
                 print(error)
 
-    def Read(self, url):
+    @staticmethod
+    def Read(url):
+        if UrlDumper.__initialized == False:
+            raise Exception('call init before writing')
         global USE_LEVELDB
         if USE_LEVELDB == True:
-            UrlDumper.__db.Get(url)
+            UrlDumper.__dbLock.acquire()
+            page = UrlDumper.__db.Get(url)
+            UrlDumper.__dbLock.release()
+            return page
         else:
             fileName = Converter.UrlToFileName(url)
             try:
-                with open(UrlDumper.__path + fileName, 'r') as pageFile:
-                     return pageFile.read()
+                absolutePath = UrlDumper.__path + '/' + fileName
+                print 'read from file', absolutePath
+                with open(absolutePath, 'r') as pageFile:
+                    return pageFile.read()
             except IOError as error:
                 print(error)
                 return None
--- crawler/UrlDumperTest.py
+++ crawler/UrlDumperTest.py
@@ -1,1 +1,35 @@
+#!/usr/bin/python
+
+import os
+import shutil
+
 from UrlDumper import UrlDumper
+from UrlDumper import USE_LEVELDB
+
+if __name__ == '__main__':
+    if USE_LEVELDB == True:
+        UrlDumper.Init('.', 'urlPages')
+    else:
+        os.mkdir('./test')
+        UrlDumper.Init('./test', None)
+
+    url1 = 'http://www.test1.com'
+    page1 = 'this is test page1'
+    UrlDumper.Write(url1, page1)
+    url2 = 'http://www.test2.com'
+    page2 = 'this is test page2'
+    UrlDumper.Write(url2, page2)
+    url3 = 'http://www.test3.com'
+    page3 = 'this is test page3'
+    UrlDumper.Write(url3, page3)
+
+    assert UrlDumper.Read(url1) == page1
+    assert UrlDumper.Read(url2) == page2
+    assert UrlDumper.Read(url3) == page3
+
+    if USE_LEVELDB == True:
+        pass
+        #print 'remove ./urlPages'
+        #shutil.rmtree('./urlPages')
+    else:
+        shutil.rmtree('./test')
