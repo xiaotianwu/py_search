5acfb6e0dd2f0892be715bb6e457ec626bfc3215
xiaotianwu <wxt1984@gmail.com>
2014-03-14 01:08:03 -0700

    add more ut

--- common/HtmlParserTest.py
+++ common/HtmlParserTest.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python
 
 import logging
+import unittest
 
 from HtmlParser import LinkExtractor
 from HtmlParser import TermExtractor
 
-if __name__ == '__main__':
-    parser = LinkExtractor();
-    parser.feed(open('../test_data/Sample.yahoo.html','r').read())
-    print('link list =', parser.link)
-    print('----------------Split---------------------')
-    parser.close()
+class HtmlPaserTest(unittest.TestCase):
+    def testLinkExtractor(self):
+        parser = LinkExtractor();
+        parser.feed(open('../test_data/Sample.yahoo.html','r').read())
+        print('link list =', parser.link)
+        print('----------------Split---------------------')
+        parser.close()
 
-    parser = TermExtractor()
-    parser.SetStopwords('../config/StopWordsList.txt')
-    print('stop words = ', parser.stopwords)
-    fileName = '../test_data/Sample.yahoo.html'
-    parser.feed(open(fileName, 'r').read())
-    print('term list =', parser.term)
-    parser.close()
+    def testTermExtractor(self):
+        parser = TermExtractor()
+        parser.SetStopwords('../config/StopWordsList.txt')
+        print('stop words = ', parser.stopwords)
+        fileName = '../test_data/Sample.yahoo.html'
+        parser.feed(open(fileName, 'r').read())
+        print('term list =', parser.term)
+        parser.close()
+
+if __name__ == '__main__':
+    unittest.main()
