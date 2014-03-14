95b79e3ff8aac7a74d227331ea3d31f88fd2a01f
xiaotianwu <wxt1984@gmail.com>
2014-03-14 00:11:27 -0700

    file forgot to commit

--- common/IORequestType.py
+++ common/IORequestType.py
@@ -2,13 +2,11 @@ import uuid
 
 class IORequest:
     '''it's for KV file'''
-    def __init__(self, requestType, fileName,
-                 key, fileType = 'UncompressIndex'):
+    def __init__(self, requestType, fileName, key):
         self.type = requestType
         self.fileName = fileName
         self.key = key
         self.result = None
-        self.Id = str(uuid.uuid4())
+        self.id = str(uuid.uuid4())
         self.retCode = 0 # TODO use enum to replace it
         self.finishEvent = None
-        self.fileType = fileType
