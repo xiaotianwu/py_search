050d2bb364f524eebc755c07c8a4e4e88c24c28f
xiaotianwu <wxt1984@gmail.com>
2014-03-16 04:09:17 -0700

    add RBTree prototype and ut for rotation, other function not tested yet

---
+++ common/RBTree.py
@@ -0,0 +1,131 @@
+RED = True
+BLACK = False
+
+class RBTreeNode:
+    __slots__ = ['key', 'val', 'color', 'left', 'right', 'parent']
+    
+    def __init__(self, key, val = None):
+        self.key = key
+        self.val = val
+        self.color = RED
+        self.left = None
+        self.right = None
+        self.parent = None
+
+class RBTree:
+    '''key is unique'''
+    def __init__(self, root):
+        self._root = root
+
+    def root(self):
+        return self._root
+
+    def Insert(self, key, val):
+        newNode = RBTreeNode(key, val)
+        insertPos = self._SearchInsertPos(self._root, None, key)
+        if insertPos.key > key:
+            assert insertPos.left == None
+            insertPos.left = newNode
+            newNode.parent = insertPos
+        else:
+            assert insertPos.right == None
+            insertPos.right = newNode
+            newNode.parent = insertPos
+        self._InsertFix(newNode)
+        
+    def Search(self, key):
+        return self._Search(node, key)
+
+    def _InsertFix(self, node):
+        while True:
+            if node == self._root:
+                node.color = BLACK
+                break
+            parent = node.parent
+            if parent.color == BLACK:
+                break
+            grandParent = parent.parent
+            if granParent.left == parent:
+                uncle = grandParent.right
+            else:
+                uncle = grandParent.left
+            if parent.color == RED:
+                if uncle == None or uncle.color == BLACK:
+                    if node == parent.right:
+                        self._LeftRotate(parent)
+                        node = parent
+                    else:
+                        parent.color = BLACK
+                        grandParent.color = RED
+                        if grandPrant.left == parent:
+                            self._RightRotate(grandParent)
+                        else:
+                            self._LeftRotate(grandParent)
+                elif uncle.color == RED:
+                    parent.color = BLACK
+                    uncle.color = BLACK
+                    grandParent.color = RED
+                    node = grandParent
+
+    def _LeftRotate(self, node):
+        assert node.right != None
+        if node == self._root:
+            self._root = node.right
+        rightChild = node.right
+        parent = node.parent
+        if parent != None:
+            if parent.left == node:
+                parent.left = rightChild
+            else:
+                parent.right = rightChild
+        rightChild.parent = parent
+        node.right = rightChild.left
+        if rightChild.left != None:
+            rightChild.left.parent = node
+        rightChild.left = node
+        node.parent = rightChild
+
+    def _RightRotate(self, node):
+        assert node.left != None
+        if node == self._root:
+            self._root = node.left
+        leftChild = node.left
+        parent = node.parent
+        if parent != None:
+            if parent.left == node:
+                parent.left = leftChild
+            else:
+                parent.right = leftChild
+        leftChild.parent = parent
+        node.right = leftChild.right
+        if leftChild.right != None:
+            leftChild.right.parent = node
+        leftChild.right = node
+        node.parent = leftChild
+
+    def _Search(self, node, key):
+        if node == None:
+            return None
+        elif node.key == key:
+            return node.val
+        elif node.key > key:
+            return _Search(node.left, key)
+        elif node.key < key:
+            return _Search(node.right, key)
+       
+    def _SearchInsertPos(self, node, parentNode, key):
+        if node == None:
+            return parentNode
+        elif node.key > key:
+            return _SearchInsertPos(node.left, node, key)
+        elif node.key < key:
+            return _SearchInsertPos(node.right, node, key)
+
+    def Delete(self, key):
+        pass
+
+    def _Delete(self, key):
+        pass
+
+    def _DeleteFix(self, key):
+        pass
---
+++ common/RBTreeTest.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+import unittest
+
+from RBTree import *
+
+class RBTreeTest(unittest.TestCase):
+    def testRotation(self):
+        root = RBTreeNode(4)
+        root.right = RBTreeNode(6)
+        root.right.parent = root
+        tree = RBTree(root)
+        tree._LeftRotate(tree.root())
+        self.assertEqual(tree.root().key, 6)
+        self.assertEqual(tree.root().parent, None)
+        self.assertEqual(tree.root().right, None)
+        self.assertEqual(tree.root().left.key, 4)
+        self.assertEqual(tree.root().left.parent.key, 6)
+        self.assertEqual(tree.root().left.left, None)
+        self.assertEqual(tree.root().left.right, None)
+
+        root = RBTreeNode(4)
+        root.right = RBTreeNode(6)
+        root.right.parent = root
+        root.right.left = RBTreeNode(5)
+        root.right.left.parent = root.right
+        root.right.right = RBTreeNode(7)
+        root.right.right.parent = root.right
+        tree = RBTree(root)
+        tree._LeftRotate(tree.root())
+        self.assertEqual(tree.root().key, 6)
+        self.assertEqual(tree.root().left.key, 4)
+        self.assertEqual(tree.root().left.parent.key, 6)
+        self.assertEqual(tree.root().left.left, None)
+        self.assertEqual(tree.root().left.right.key, 5)
+        self.assertEqual(tree.root().left.right.parent.key, 4)
+        self.assertEqual(tree.root().left.right.left, None)
+        self.assertEqual(tree.root().left.right.right, None)
+        self.assertEqual(tree.root().right.key, 7)
+        self.assertEqual(tree.root().right.parent.key, 6)
+        self.assertEqual(tree.root().right.left, None)
+        self.assertEqual(tree.root().right.right, None)
+
+        root = RBTreeNode(4)
+        root.right = RBTreeNode(6)
+        root.right.parent = root
+        root.right.left = RBTreeNode(5)
+        root.right.left.parent = root.right
+        root.right.right = RBTreeNode(8)
+        root.right.right.parent = root.right
+        root.right.right.left = RBTreeNode(7)
+        root.right.right.left.parent = root.right.right
+        root.right.right.right = RBTreeNode(9)
+        root.right.right.right.parent = root.right.right
+        tree = RBTree(root)
+        tree._LeftRotate(root.right)
+        self.assertEqual(tree.root().key, 4)
+        self.assertEqual(tree.root().left, None)
+        self.assertEqual(tree.root().right.key, 8)
+        self.assertEqual(tree.root().right.parent.key, 4)
+        self.assertEqual(tree.root().right.left.key, 6)
+        self.assertEqual(tree.root().right.left.left.key, 5)
+        self.assertEqual(tree.root().right.left.left.left, None)
+        self.assertEqual(tree.root().right.left.left.right, None)
+        self.assertEqual(tree.root().right.left.right.key, 7)
+        self.assertEqual(tree.root().right.left.right.left, None)
+        self.assertEqual(tree.root().right.left.right.right, None)
+        self.assertEqual(tree.root().right.right.key, 9)
+        self.assertEqual(tree.root().right.right.left, None)
+        self.assertEqual(tree.root().right.right.right, None)
+
+if __name__ == '__main__':
+    unittest.main()
