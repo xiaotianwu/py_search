0ae2c5159a73b9501ae427eb96fa426ca998d53f
xiaotianwu <wxt1984@gmail.com>
2014-03-17 00:10:19 -0700

    add block manager and test

--- index_serve/IndexBlockManager.py
+++ index_serve/IndexBlockManager.py
@@ -1,25 +1,37 @@
-from IndexConfig import INDEXFOLDER
-from IndexConfig import INDEXMAPPING
+from common.RBTree import RBTree
 
 class IndexBlock:
     def __init__(self, docidStart, docidEnd, mappingFile, blockType):
-        self._docidStart = docidStart
-        self._docidEnd = docidEnd
-        self._mappingFile = mappingFile
-        self._blockType = blockType
+        self.docidStart = docidStart
+        self.docidEnd = docidEnd
+        self.mappingFile = mappingFile
+        self.blockType = blockType
 
 class IndexBlockManager:
-    def __init__(self):
-        self._indexFolder = INDEXFOLDER
-        self._indexMappingRawStr = INDEXMAPPING
-        self._ParseIndexMappingStr(INDEXMAPPING)
+    def __init__(self, folder, mappingStr):
+        self._indexFolder = folder
+        self._indexMappingRawStr = mappingStr
+        self._blockTree = RBTree()
+        self._ParseIndexMappingStr()
 
-    def _ParseIndexMappingStr(rawStr):
+    def _ParseIndexMappingStr(self):
+        rawStr = self._indexMappingRawStr
         allMapping = rawStr.strip().split(';')
         for mapping in allMapping:
             mapping = mapping.split(':')
             assert len(mapping) >= 3
-            docidRange = mapping[0][1:-2].split(',')
-            block = IndexBlock(int(docidRange[0]),
+            docidRange = mapping[0].split(',')
+            key = int(docidRange[0])
+            block = IndexBlock(key,
                                int(docidRange[1]),
                                mapping[1], mapping[2])
+            self._blockTree.Insert(key, block)
+
+    def GetIndexBlock(self, termId):
+        key, block = self._blockTree.LowerBound(termId)
+        if block == None:
+            return None
+        if termId >= block.docidStart and termId <= block.docidEnd:
+            return block
+        else:
+            return None
