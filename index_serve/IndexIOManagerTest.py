f4fd4bcdf7d12971e30f656bb546e90378aa1336
xiaotianwu <wxt1984@gmail.com>
2014-03-16 21:46:22 -0700

    add iomanger unittest

--- index_serve/IndexIOManagerTest.py
+++ index_serve/IndexIOManagerTest.py
@@ -5,80 +5,72 @@ import random
 import shutil
 #import gevent
 import threading
+import unittest
 
+from common.Common import GenRandomIndex
 from common.IORequestType import IORequest
-from common.uncompress_index.UncompressIndex import UncompressIndexWriter
 from common.uncompress_index.UncompressIndex import UncompressIndex
+from common.uncompress_index.UncompressIndex import UncompressIndexWriter
 from IndexIOManager import IndexIOManagerThread
 
-def GenRandomIndex():
-    i = 0
-    s = set()
-    while i < 1000:
-        (item1, item2) = (random.randint(0, 10000),
-                          random.randint(0, 10000))
-        if (item1, item2) in s:
-            continue
-        else:
-            s.add((item1, item2))
-            i += 1
-    return s
+class IndexIOManagerTest(unittest.TestCase):
+    _index = None
 
-def CreateTestData(begin, end):
-    if os.path.exists('testdata'):
-        shutil.rmtree('testdata')
-    os.mkdir('testdata')
-    index = UncompressIndex()
-    for i in range(0, 10000):
-        index.Add(i, GenRandomIndex())
+    def CreateTestData(self, begin, end):
+        if os.path.exists('testdata'):
+            shutil.rmtree('testdata')
+        os.mkdir('testdata')
+        index = UncompressIndex()
+        for i in range(0, 10000):
+            index.Add(i, GenRandomIndex())
+    
+        fileNameBase = 'testdata/testFile' + str(begin)
+        print('create index file' + fileNameBase)
+        writer = UncompressIndexWriter()
+        writer.Write(index, fileNameBase)
+    
+        for i in range(begin + 1, end):
+            fileName = 'testdata/testFile' + str(i)
+            cp = 'cp ' + fileNameBase + ' ' + fileName
+            print cp
+            os.system(cp)
+    
+        return index
 
-    fileNameBase = 'testdata/testFile' + str(begin)
-    print('create index file' + fileNameBase)
-    writer = UncompressIndexWriter()
-    writer.Write(index, fileNameBase)
+    def setUp(self):
+        self._index = self.CreateTestData(0, 10)
+        #manager = IndexIOManager()
+        #gevent.spawn(manager.run)
+        self._ioManagerThread = IndexIOManagerThread(4, 5000)
+        #self._ioManagerThread = IndexIOManagerThread(4, 0)
+        self._ioManagerThread.start()
 
-    for i in range(begin + 1, end):
-        fileName = 'testdata/testFile' + str(i)
-        cp = 'cp ' + fileNameBase + ' ' + fileName
-        print cp
-        os.system(cp)
+    def tearDown(self):
+        os.system('rm testdata -r')  
 
-    return index
+    def testRandomRead(self):
+        events = []
+        requests = []
 
-def DeleteTestData():
-    os.system('rm testdata -r')  
+        for i in range(0, 5000):
+            fileName = 'testdata/testFile' + str(i % 10)
+            req = IORequest('READ', fileName,
+                            random.randint(0, 9999))
+            requests.append(req)
+            ev = self._ioManagerThread.PostIORequest(req)
+            events.append(ev)
+
+        for ev in events:
+            ev.wait()
+
+        for req in requests:
+            self.assertEqual(req.result, self._index.Fetch(req.key))
+        
+        #manager.PostStopRequest()
+        self._ioManagerThread.PostStopRequest()
+        self._ioManagerThread.join()
+
+        print 'Cache Hit Ratio:', self._ioManagerThread._manager.CacheHitRatio()
 
 if __name__ == '__main__':
-    randomIndex = CreateTestData(0, 10)
-
-    #manager = IndexIOManager()
-    #gevent.spawn(manager.run)
-    dmThread = IndexIOManagerThread(4, 5000)
-    #dmThread = IndexIOManagerThread(4, 0)
-    dmThread.start()
-    
-    events = []
-    requests = []
-
-    for i in range(0, 5000):
-        fileName = 'testdata/testFile' + str(i % 10)
-        req = IORequest('READ', fileName,
-                        random.randint(0, 9999))
-        requests.append(req)
-        ev = dmThread.PostIORequest(req)
-        events.append(ev)
-
-    for ev in events:
-        ev.wait()
-
-    for req in requests:
-        assert req.result == randomIndex.Fetch(req.key)
-
-    print('test all done')
-    DeleteTestData()
-    
-    #manager.PostStopRequest()
-    dmThread.PostStopRequest()
-    dmThread.join()
-
-    print 'Cache Hit Ratio:', dmThread._manager.CacheHitRatio()
+    unittest.main()
